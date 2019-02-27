e3bb7c5082361373852b728a24c36b3746c7f177
Nikolai Roede Dokken <nikolard@stud.ntnu.no>
2019-02-27 16:23:30 +0100

    main menu

--- src/Scenes/MainMenu.java
+++ src/Scenes/MainMenu.java
@@ -20,95 +20,41 @@ public class MainMenu {
 
     public MainMenu(double width, double height) {
         gp = new GridPane();
-        addUIcontrols(gp);
-        sc = (gp, width, height);
+        addUIControls(gp);
+        sc = new Scene(gp, width, height);
     }
 
     public Scene getScene() {
-        return mainMenu;
+        return sc;
     }
 
-    private GridPane createRegistrationFormPane() {
-        // Instantiate a new Grid Pane
-        GridPane gridPane = new GridPane();
 
-        // Position the pane at the center of the screen, both vertically and horizontally
-        gridPane.setAlignment(Pos.CENTER);
-
-        // Set a padding of 20px on each side
-        gridPane.setPadding(new Insets(40, 40, 40, 40));
-
-        // Set the horizontal gap between columns
-        gridPane.setHgap(10);
-
-        // Set the vertical gap between rows
-        gridPane.setVgap(10);
-
-
-        // Add Column Constraints
-
-        // columnOneConstraints will be applied to all the nodes placed in column one.
-        ColumnConstraints columnOneConstraints = new ColumnConstraints(100, 100, Double.MAX_VALUE);
-        columnOneConstraints.setHalignment(HPos.RIGHT);
-
-        // columnTwoConstraints will be applied to all the nodes placed in column two.
-        ColumnConstraints columnTwoConstrains = new ColumnConstraints(200,200, Double.MAX_VALUE);
-        columnTwoConstrains.setHgrow(Priority.ALWAYS);
-
-        gridPane.getColumnConstraints().addAll(columnOneConstraints, columnTwoConstrains);
-
-        return gridPane;
-    }
     private void addUIControls(GridPane gridPane) {
-
         double prefHeight = 40;
+
         // Add Header
-        Label headerLabel = new Label("Registration Form");
+        Label headerLabel = new Label("Main Menu");
         headerLabel.setFont(Font.font("Arial", FontWeight.BOLD, 24));
         gridPane.add(headerLabel, 0,0,2,1);
         GridPane.setHalignment(headerLabel, HPos.CENTER);
         GridPane.setMargin(headerLabel, new Insets(20, 0,20,0));
 
-        // Add Name Label
-        Label nameLabel = new Label("Full Name : ");
-        gridPane.add(nameLabel, 0,1);
+        // Join game button
+        Button joinGameButton = new Button("Join Game");
+        gridPane.add(joinGameButton, 0,1);
 
-        // Add Name Text Field
-        TextField nameField = new TextField();
-        nameField.setPrefHeight(prefHeight);
-        gridPane.add(nameField, 1,1);
+        // Options game button
+        Button optionButton = new Button("Options");
+        gridPane.add(optionButton, 1,1);
 
-
-        // Add Email Label
-        Label emailLabel = new Label("Email ID : ");
-        gridPane.add(emailLabel, 0, 2);
-
-        // Add Email Text Field
-        TextField emailField = new TextField();
-        emailField.setPrefHeight(prefHeight);
-        gridPane.add(emailField, 1, 2);
-
-        // Add Password Label
-        Label passwordLabel = new Label("Password : ");
-        gridPane.add(passwordLabel, 0, 3);
-
-        // Add Password Field
-        PasswordField passwordField = new PasswordField();
-        passwordField.setPrefHeight(prefHeight);
-        gridPane.add(passwordField, 1, 3);
-
-        // Add Submit Button
-        Button submitButton = new Button("Submit");
-        submitButton.setPrefHeight(prefHeight);
-        submitButton.setDefaultButton(true);
-        submitButton.setPrefWidth(100);
-        gridPane.add(submitButton, 0, 4, 2, 1);
-        GridPane.setHalignment(submitButton, HPos.CENTER);
-        GridPane.setMargin(submitButton, new Insets(20, 0,20,0));
+        // Quit button
+        Button quitButton = new Button("Quit");
+        quitButton.setPrefHeight(prefHeight);
+        quitButton.setDefaultButton(true);
+        quitButton.setPrefWidth(100);
+        gridPane.add(quitButton, 0, 4, 2, 1);
+        GridPane.setHalignment(quitButton, HPos.CENTER);
+        GridPane.setMargin(quitButton, new Insets(20, 0,20,0));
     }
-}
-
-
-
 }
 
