161052293bcb5400892b529d761eb7affe3fb7de
kaspervg <kvgun@live.no>
2019-02-27 16:40:48 +0100

    prlver å fikse noe

--- src/Main.java
+++ src/Main.java
@@ -7,23 +7,24 @@ public class Main extends Application {
 
     private static final double HEIGHT = 600;
     private static final double WIDTH = 1000;
-    Stage stage;
+    Stage window;
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
     public void start(Stage stage) {
-        stage.setTitle("Squiggle");
-        stage.setOnCloseRequest(e -> {
+        window = stage;
+        window.setTitle("Squiggle");
+        window.setOnCloseRequest(e -> {
             e.consume();
             closeProgram();
         });
 
         Pane layout = new Pane();
         Scene scene = new Scene(layout, WIDTH, HEIGHT);
-        stage.setScene(scene);
-        stage.show();
+        window.setScene(scene);
+        window.show();
 
 
     }
@@ -31,7 +32,7 @@ public class Main extends Application {
     private void closeProgram(){
         Boolean answer = ConfirmBox.display("Feilmelding!!", "Sure you want to exit?");
         if(answer){
-            stage.close();
+            window.close();
         }
     }
 }
