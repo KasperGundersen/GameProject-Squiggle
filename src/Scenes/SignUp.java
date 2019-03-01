46a60ce2b8983361966b178257a9a56e6f23b60c
Zenjjim <zaim.imran@gmail.com>
2019-03-01 15:08:52 +0100

    fikset feilmelding på scene signup

--- src/Scenes/SignUp.java
+++ src/Scenes/SignUp.java
@@ -9,8 +9,11 @@ import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
 import javafx.scene.text.FontWeight;
+import javafx.scene.text.TextAlignment;
 
 public class SignUp extends Scenes {
 
@@ -20,9 +23,19 @@ public class SignUp extends Scenes {
     private static PasswordField passwordField;
     private static PasswordField rePasswordField;
 
+    private static Label errorUsernMail;
+    private static Label errorPassword;
+
     private Button submitButton;
     private Button optionButton;
     private Button backButton;
+
+    public static void visiableUserMail(boolean b){
+        errorUsernMail.setVisible(b);
+    }
+    public static void visiablePassword(boolean b){
+        errorPassword.setVisible(b);
+    }
 
     //construktør fra super
     public SignUp(double WIDTH, double HEIGHT) {
@@ -65,6 +78,17 @@ public class SignUp extends Scenes {
         GridPane.setHalignment(headerLabel, HPos.CENTER);
         GridPane.setMargin(headerLabel, new Insets(20, 0,20,0));
 
+        //Add error Label
+        errorUsernMail = new Label("Username or email already taken");
+        gridPane.add(errorUsernMail,1,0,2,2);
+        errorUsernMail.setVisible(false);
+        errorUsernMail.setTextFill(Color.RED);
+        errorUsernMail.setFont(Font.font(
+                "Arial",
+                FontPosture.ITALIC,
+                Font.getDefault().getSize()
+        ));
+
         // Add Name Label
         Label nameLabel = new Label("Username : ");
         gridPane.add(nameLabel, 0,1);
@@ -85,7 +109,19 @@ public class SignUp extends Scenes {
         emailField.setPromptText("party@myhouse.tonight");
         gridPane.add(emailField, 1, 2);
 
-        // Add Password Label
+        //Add error Label
+        errorPassword = new Label("Password don't match");
+        gridPane.add(errorPassword,1,2,2,2);
+        GridPane.setMargin(headerLabel, new Insets(10, 0,10,0));
+        errorPassword.setVisible(false);
+        errorPassword.setTextFill(Color.RED);
+        errorPassword.setFont(Font.font(
+                "Arial",
+                FontPosture.ITALIC,
+                Font.getDefault().getSize()
+        ));
+
+        // Add Passfword Label
         Label passwordLabel = new Label("Password : ");
         gridPane.add(passwordLabel, 0, 3);
 
@@ -93,6 +129,7 @@ public class SignUp extends Scenes {
         passwordField = new PasswordField();
         passwordField.setPrefHeight(prefHeight);
         passwordField.setPromptText("password");
+        GridPane.setMargin(passwordField, new Insets(10, 0,0,0));
         gridPane.add(passwordField, 1, 3);
 
         // Add RePassword Label
@@ -116,20 +153,18 @@ public class SignUp extends Scenes {
 
         // Add option button
         optionButton = new Button("Options");
-        gridPane.add(optionButton, 5, 14);
-        optionButton.setOnAction(e -> Options.openOptions());
-
+        gridPane.add(optionButton, 3, 6);
+        GridPane.setHalignment(optionButton, HPos.LEFT);
+        GridPane.setValignment(optionButton, VPos.BOTTOM);
         // Go back button
         backButton = new Button("Go Back");
-        backButton.setPrefHeight(prefHeight);
-        backButton.setDefaultButton(true);
-        backButton.setPrefWidth(100);
-        gridPane.add(backButton, 0, 5, 1, 2);
-        GridPane.setHalignment(backButton, HPos.CENTER);
-        GridPane.setValignment(backButton, VPos.CENTER);
+        gridPane.add(backButton, 0, 6);
+        GridPane.setHalignment(backButton, HPos.LEFT);
+        GridPane.setValignment(backButton, VPos.BOTTOM);
 
         // Button submition
         super.buttonChangeScene(backButton, MainScene.li);
         submitButton.setOnAction(e -> Authentication.submit());
+        optionButton.setOnAction(e -> Options.openOptions());
     }
 }
