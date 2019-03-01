c9779ad4f6d289c29f971f947e98ecd312cc4e5f
kaspervg <kvgun@live.no>
2019-03-01 17:53:10 +0100

    confirm box

--- src/Scenes/ConfirmBox.java
+++ src/Scenes/ConfirmBox.java
@@ -5,23 +5,30 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.sql.Connection;
 
-public class ConfirmBox {
+public class ConfirmBox{
 
     static boolean ansver;
+    private static GridPane grid;
 
     public static boolean display(String title, String message){
         Stage stage = new Stage();
+
+        grid = new GridPane();
+        grid.setAlignment(Pos.TOP_CENTER);
 
         stage.initModality(Modality.APPLICATION_MODAL);
         stage.setTitle(title);
         stage.setMinWidth(250);
         stage.setMinHeight(250);
+
         Label label = new Label();
         label.setText(message);
 
@@ -41,11 +48,14 @@ public class ConfirmBox {
             stage.close();
         });
 
-        VBox layout = new VBox(10);
-        layout.getChildren().addAll(label, yesButton, noButton);
+        HBox layout = new HBox(10);
+        layout.getChildren().addAll(yesButton, noButton);
         layout.setAlignment(Pos.CENTER);
 
-        Scene scene = new Scene(layout);
+        grid.add(label, 0, 0);
+        grid.add(layout,0,2);
+
+        Scene scene = new Scene(grid);
 
         stage.initModality(Modality.APPLICATION_MODAL);
         stage.setScene(scene);
