52063648461f4f5b760c40f3bf36dfb050be8b0d
Nikolai Roede Dokken <nikolard@stud.ntnu.no>
2019-03-05 15:27:26 +0100

    Now closing statements and resulstsets

--- src/Database/DBConnection.java
+++ src/Database/DBConnection.java
@@ -17,7 +17,6 @@ public class DBConnection {
 
     // Standard JDBC components
     private static Connection con;
-    private static ResultSet res;
 
     // Use this whenever you want to connect to the database
     public static Connection getCon() {
@@ -35,9 +34,10 @@ public class DBConnection {
     // Method that registers a user
     public static void registerUser(String userName, String hash, String salt, String userEmail, int avatarID) {
         Connection con = getCon();
+        PreparedStatement prepStmt = null;
         try {
             String query = "INSERT INTO USERS VALUES (0, ?, ?, ?, ?, ?, 0)";
-            PreparedStatement prepStmt = con.prepareStatement(query);
+            prepStmt = con.prepareStatement(query);
             prepStmt.setString(1, userName);
             prepStmt.setString(2, hash);
             prepStmt.setString(3, salt);
@@ -48,16 +48,19 @@ public class DBConnection {
             e.printStackTrace();
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            closeConnection(con, prepStmt);
         }
-        closeConnection(con);
     }
 
     // This method looks for "input" in the given column in the database
     public static boolean exists(String columnName, String input) {
         Connection con = getCon();
+        PreparedStatement prepStmt = null;
+        ResultSet res = null;
         try{
             String query = "SELECT " + columnName + " FROM USERS WHERE " + columnName + "= ?;";
-            PreparedStatement prepStmt = con.prepareStatement(query);
+            prepStmt = con.prepareStatement(query);
             prepStmt.setString(1, input);
             res = prepStmt.executeQuery();
             return res.next();
@@ -65,32 +68,37 @@ public class DBConnection {
             e.printStackTrace();
         } catch (NullPointerException e) {
             e.printStackTrace();
+        } finally {
+            closeConnection(con, prepStmt, res);
+            return false;
         }
-        closeConnection(con);
-        return false;
     }
 
     // Makes a user show as logged in when logged in
     public static void setLoggedIn(String username, int loggedIn) {
         Connection con = getCon();
+        PreparedStatement prepStmt = null;
         try {
             String query = "UPDATE USERS SET loggedIn=? WHERE userName=?;";
-            PreparedStatement prepStmt = con.prepareStatement(query);
+            prepStmt = con.prepareStatement(query);
             prepStmt.setInt (1, loggedIn);
             prepStmt.setString(2, username);
             prepStmt.executeUpdate();
         } catch(SQLException e) {
             e.printStackTrace();
+        } finally {
+            closeConnection(con, prepStmt);
         }
-        closeConnection(con);
     }
 
     // Gets salt, used for comparing passwords
     public static String getSalt(String username) {
         Connection con = getCon();
+        PreparedStatement prepStmt = null;
+        ResultSet res = null;
         try {
             String query = "SELECT salt FROM USERS WHERE userName=?;";
-            PreparedStatement prepStmt = con.prepareStatement(query);
+            prepStmt = con.prepareStatement(query);
             prepStmt.setString(1, username);
             res = prepStmt.executeQuery();
             if(res.next()) {
@@ -99,18 +107,21 @@ public class DBConnection {
             }
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            closeConnection(con, prepStmt, res);
+            return null;
         }
-        closeConnection(con);
-        return null;
     }
 
     // For seing if a user is already logged in or not
     public static boolean getLoggedIn(String username) {
         boolean loggedIn = false;
         Connection con = getCon();
+        PreparedStatement prepStmt = null;
+        ResultSet res = null;
         try {
             String query = "SELECT loggedIn FROM USERS WHERE userName=?;";
-            PreparedStatement prepStmt = con.prepareStatement(query);
+            prepStmt = con.prepareStatement(query);
             prepStmt.setString(1, username);
             res = prepStmt.executeQuery();
             res.next();
@@ -118,16 +129,50 @@ public class DBConnection {
             loggedIn = (num == 0 ? false : true);
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            closeConnection(con, prepStmt, res);
+            return loggedIn;
         }
-        closeConnection(con);
-        return loggedIn;
     }
 
     // General method for closing a connection, is to be used everytime getCon() is used
-    public static void closeConnection(Connection con) {
+    public static void closeConnection(Connection con, Statement stmt, ResultSet res) {
         try {
-            con.close();
-        } catch (SQLException e) {
+            if (res != null) {
+                res.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+            if (stmt != null) {
+                stmt.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+            if (con != null) {
+                con.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void closeConnection(Connection con, Statement stmt) {
+        try {
+            if (stmt != null) {
+                stmt.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+            if (con != null) {
+                con.close();
+            }
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
@@ -135,24 +180,27 @@ public class DBConnection {
     // Sets avatarID in the database, making the user have same avatarID on next LogIn
     public static void setAvatarID(int userID, int index) {
         Connection con = getCon();
+        PreparedStatement prepStmt = null;
         try {
             String query = "UPDATE USERS SET avatarID=? WHERE UserID=?";
-            PreparedStatement prepStmt = con.prepareStatement(query);
+            prepStmt = con.prepareStatement(query);
             prepStmt.setInt(1, index);
             prepStmt.setInt(2, userID);
             prepStmt.executeUpdate();
-
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            closeConnection(con, prepStmt);
         }
-        closeConnection(con);
     }
 
     public static ArrayList<String> getWords(String category) {
         Connection con = getCon();
+        PreparedStatement prepStmt = null;
+        ResultSet res = null;
         try {
             String query = "SELECT word FROM LIBRARY WHERE category=?;";
-            PreparedStatement prepStmt = con.prepareStatement(query);
+            prepStmt = con.prepareStatement(query);
             prepStmt.setString(1, category);
             res = prepStmt.executeQuery();
             ArrayList<String> wordList = new ArrayList<>();
@@ -162,19 +210,21 @@ public class DBConnection {
             }
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            closeConnection(con, prepStmt, res);
+            return null;
         }
-        closeConnection(con);
-        return null;
-
     }
 
 
     // Fetches userID given username, used upon initialization of user, log in
     public static int getUserID(String username) {
         Connection con = getCon();
+        PreparedStatement prepStmt = null;
+        ResultSet res = null;
         try {
             String query = "SELECT UserID FROM USERS WHERE userName=?;";
-            PreparedStatement prepStmt = con.prepareStatement(query);
+            prepStmt = con.prepareStatement(query);
             prepStmt.setString (1, username);
             res = prepStmt.executeQuery();
             if (res.next()) {
@@ -182,18 +232,21 @@ public class DBConnection {
             }
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            closeConnection(con, prepStmt, res);
+            return 0;
         }
-        closeConnection(con);
-        return 0;
     }
 
 
     // Method that runs on "Join Game", sets drawing to 1, if no one else is ingame
     public static void setDrawer() {
         Connection con = getCon();
+        PreparedStatement prepStmt = null;
+        ResultSet res = null;
         try {
             String query = "SELECT * FROM GAME WHERE drawing=1";
-            PreparedStatement prepStmt = con.prepareStatement(query);
+            prepStmt = con.prepareStatement(query);
             res = prepStmt.executeQuery();
             if (res.next()) {
                 UserInfo.setDrawing(false);
@@ -206,15 +259,17 @@ public class DBConnection {
             }
         } catch(SQLException e ) {
             e.printStackTrace();
+        } finally {
+            closeConnection(con, prepStmt, res);
         }
-        closeConnection(con);
     }
 
     public static void enterGame() {
         Connection con = getCon();
+        PreparedStatement prepStmt = null;
         try {
             String query = "INSERT INTO GAME VALUES (?, ?, ?, ?)";
-            PreparedStatement prepStmt = con.prepareStatement(query);
+            prepStmt = con.prepareStatement(query);
             prepStmt.setInt(1, UserInfo.getUserID());
             prepStmt.setInt(2, 0);
             prepStmt.setInt(3, 0);
@@ -222,26 +277,29 @@ public class DBConnection {
             prepStmt.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            closeConnection(con, prepStmt);
         }
-        closeConnection(con);
     }
 
     public static void exitGame() {
         Connection con = getCon();
+        PreparedStatement prepStmt = null;
         try {
             String query = "DELETE FROM GAME WHERE userID = ?";
-            PreparedStatement prepStmt = con.prepareStatement(query);
+            prepStmt = con.prepareStatement(query);
             prepStmt.setInt(1, UserInfo.getUserID());
             prepStmt.executeUpdate();
         } catch(SQLException e) {
             e.printStackTrace();
         }
-        closeConnection(con);
+        closeConnection(con, prepStmt);
     }
 
 
     public static void insertIntoDB(String words) {
         Connection con = getCon();
+        PreparedStatement prepStmt = null;
         try {
             String dropTable = ""
                     + "DROP TABLE LIBRARY;";
@@ -250,53 +308,60 @@ public class DBConnection {
                     + "wordID INT(4) PRIMARY KEY AUTO_INCREMENT, "
                     + "    word VARCHAR(30) "
                     + ");";
-            Statement stmt = con.createStatement();
-            stmt.executeUpdate(dropTable);
-            stmt.executeUpdate(createTable);
-            stmt.executeUpdate("INSERT INTO LIBRARY VALUE (default, \"" +  words + "\");");
+            prepStmt = con.prepareStatement(dropTable);
+            prepStmt.executeUpdate();
+            prepStmt = con.prepareStatement(createTable);
+            prepStmt.executeUpdate();
+            String insert = "INSERT INTO LIBRARY VALUE (default, \"" +  words + "\");";
+            prepStmt = con.prepareStatement(insert);
+            prepStmt.executeUpdate();
         } catch (SQLSyntaxErrorException e) {
             e.printStackTrace();
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            closeConnection(con, prepStmt);
         }
-        closeConnection(con);
     }
-
 
     //Livechat methods start
     public static void insertMessage(String message) {
         Connection con = getCon();
+        PreparedStatement prepStmt = null;
         try {
             String query = "INSERT INTO CHAT VALUE (default, ?, ?);";
-            PreparedStatement prepStmt = con.prepareStatement(query);
+            prepStmt = con.prepareStatement(query);
             prepStmt.setInt (1, UserInfo.getUserID());
             prepStmt.setString (2, message);
             prepStmt.executeUpdate();
         } catch(SQLException e) {
             e.printStackTrace();
         } finally {
-            closeConnection(con);
+            closeConnection(con, prepStmt);
         }
     }
 
     public static ArrayList<String> getMessages() {
         Connection con = getCon();
+        PreparedStatement prepStmt = null;
+        ResultSet res = null;
         try {
-            Statement stmt = con.createStatement();
-            ResultSet res = stmt.executeQuery("select input from CHAT");
+            String query = "SELECT input FROM CHAT";
+            prepStmt = con.prepareStatement(query);
+            res = prepStmt.executeQuery();
             ArrayList<String> messages = new ArrayList<>();
             while (res.next()) {
                 if (!(res.getString("input").equals(""))) {
                     messages.add(UserInfo.getUserName() + ": " + res.getString("input"));
                 }
             }
-            closeConnection(con);
             return messages;
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            closeConnection(con, prepStmt, res);
+            return null;
         }
-        closeConnection(con);
-        return null;
     }
 
    /* public static getUsername(int userId) {
