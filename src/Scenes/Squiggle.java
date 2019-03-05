9c6097f251dbe9e7e37abdabee93759a84f521bf
audunka <audunka@stud.ntnu.no>
2019-03-05 12:02:37 +0100

    Very basic version of game is complete, things still need to be alligned and smoothened out

--- src/Scenes/Squiggle.java
+++ src/Scenes/Squiggle.java
@@ -1,10 +1,13 @@
 package Scenes;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.VPos;
 import javafx.scene.Cursor;
+import javafx.scene.ImageCursor;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
@@ -12,10 +15,16 @@ import javafx.scene.control.ColorPicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BackgroundImage;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+
+import javax.swing.*;
+import java.awt.*;
 
 
 public class Squiggle extends Scenes{
@@ -24,11 +33,13 @@ public class Squiggle extends Scenes{
         super(width, height);
         addUIControls(super.getGp());
     }
-
-
+    private double eraserSize;
 
     private void addUIControls(GridPane gridPane){
-        //Buttons
+        Image pencil = new Image("pencil.png");
+        Image rubber = new Image("rubber.png");
+
+        /*-----------Adding draw and erase buttons------*/
         ToggleButton draw = new ToggleButton("Draw");
         ToggleButton erase = new ToggleButton("Erase");
 
@@ -42,22 +53,51 @@ public class Squiggle extends Scenes{
             tool.setCursor(Cursor.HAND);
         }
 
-        VBox btns = new VBox(10);
-        btns.getChildren().addAll(draw, erase);
-        btns.setPadding(new Insets(5));
-        btns.setStyle("-fx-background-color: #999");
-        btns.setPrefWidth(60);
+        /*------Adding buttons to change linewidth--------*/
+        ToggleButton lineWidt1 = new ToggleButton("1");
+        ToggleButton lineWidth2 = new ToggleButton("2");
+        ToggleButton lineWidth3 = new ToggleButton("3");
+        ToggleButton lineWidth4 = new ToggleButton("4");
 
-        gridPane.add(btns, 0,0);
+        ToggleButton[] penSize = {lineWidt1, lineWidth2, lineWidth3, lineWidth4};
 
+        ToggleGroup lineWidthtg = new ToggleGroup();
+
+        for(ToggleButton tb : penSize){
+            tb.setMinWidth(50);
+            tb.setToggleGroup(lineWidthtg);
+            tb.setCursor(Cursor.HAND);
+        }
+
+        /*-------------Drawing Canvas-------------*/
         Canvas canvas = new Canvas(800, 580);
-        canvas.setStyle("-fx-border-style: solid inside;" +"-fx-border-color: black;" + "-fx-border-width: 2;" + "-fx-border-radius: 5;" +
-                "-fx-border-insets: 5;" +"-fx-border-padding: 10;");
         gridPane.add(canvas, 1,1,5,5);
 
-        ColorPicker cp = new ColorPicker(Color.BLACK);
+        ColorPicker cp = new ColorPicker();
+        cp.setValue(Color.BLACK);
         GraphicsContext gc = canvas.getGraphicsContext2D();
         gc.setLineWidth(1);
+
+        /*Adding what happens when a color is selected in the color palette*/
+        cp.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                cp.setValue(cp.getValue());
+            }
+        });
+
+        /*---------Grouping buttons together in a box----------*/
+        VBox buttons = new VBox(10);
+        buttons.getChildren().addAll(draw, erase, cp, lineWidt1, lineWidth2, lineWidth3, lineWidth4);
+        buttons.setPadding(new Insets(5));
+        buttons.setStyle("-fx-background-color: #999");
+        buttons.setPrefWidth(60);
+
+        gridPane.add(buttons, 0,0);
+
+        /*-----------Drawing Logic------------*/
+
+        eraserSize = 5;
 
         canvas.setOnMousePressed(e-> {
             if (draw.isSelected()) {
@@ -66,7 +106,7 @@ public class Squiggle extends Scenes{
                 gc.lineTo(e.getX(), e.getY());
 
             }else if(erase.isSelected()){
-                gc.clearRect(e.getX() - 1, e.getY() - 1, 20, 20);
+                gc.clearRect(e.getX() - 1, e.getY() - 1, eraserSize, eraserSize);
             }
         });
 
@@ -76,7 +116,7 @@ public class Squiggle extends Scenes{
                 gc.stroke();
 
             }else if(erase.isSelected()){
-                gc.clearRect(e.getX() - 1, e.getY() -1 , 20,20);
+                gc.clearRect(e.getX() - 1, e.getY() -1 , eraserSize,eraserSize);
             }
         });
 
@@ -86,8 +126,38 @@ public class Squiggle extends Scenes{
                 gc.stroke();
                 gc.closePath();
             }else if(erase.isSelected()){
-                gc.clearRect(e.getX() - 1, e.getY() - 1, 20, 20);
+                gc.clearRect(e.getX() - 1, e.getY() - 1, eraserSize, eraserSize);
             }
+        });
+
+        /*---------Changing Cursors------------*/
+        draw.setOnAction(e->{
+            canvas.setCursor(new ImageCursor(pencil));
+        });
+
+        erase.setOnAction(e->{
+            canvas.setCursor(new ImageCursor(rubber));
+        });
+
+        /*------------Changing line width-----------*/
+        lineWidt1.setOnAction(e->{
+                gc.setLineWidth(1);
+                eraserSize = 5;
+        });
+
+        lineWidth2.setOnAction(e->{
+                gc.setLineWidth(4);
+                eraserSize = 10;
+        });
+
+        lineWidth3.setOnAction(e->{
+                gc.setLineWidth(6);
+                eraserSize = 15;
+        });
+
+        lineWidth4.setOnAction(e->{
+                gc.setLineWidth(10);
+                eraserSize = 20;
         });
     }
 }
