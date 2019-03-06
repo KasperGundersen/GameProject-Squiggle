85020de534884d0e524ea1a3d8de29810ac960db
Nikolai Roede Dokken <nikolard@stud.ntnu.no>
2019-03-06 10:10:02 +0100

    Added som tests to JUnit

--- src/JUnit_test.java
+++ src/JUnit_test.java
@@ -1,4 +1,9 @@
+import Components.Encryptor;
+import Components.UserInfo;
+import Database.DBConnection;
 import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class JUnit_test {
     private static JUnit_test instance;
@@ -6,8 +11,8 @@ public class JUnit_test {
     @BeforeAll
     public static void setUpClass() throws Exception {
         // Create objects of the classes that are to be tested
+        UserInfo test = new UserInfo();
     }
-
 
     @AfterAll
     public  static void tearDownClass() {
@@ -27,9 +32,59 @@ public class JUnit_test {
 
     // Test of given method ......
     @Test
-    public void ExampleTest() throws Exception {
-        // String actualResult = someObject.someMethod();
-        // String expectedResult = "expected result";
-        // assertEquals(actualResult, expectedResult);
+    public void userExistsTest() throws Exception {
+        String userName = "admin";
+        String columnName = "userName";
+        boolean test = DBConnection.exists(columnName, userName);
+        boolean expResult = true;
+        assertEquals(expResult, test);
     }
+
+    @Test
+    public void userNotExistsTest() throws Exception {
+        String userName = "thisUserDoesntExist";
+        String columnName = "userName";
+        boolean test = DBConnection.exists(columnName, userName);
+        boolean expResult = false;
+        assertEquals(expResult, test);
+    }
+
+    @Test
+    public void passwordExists() throws Exception {
+        String username = "admin";
+        String password = "admin";
+        String salt = DBConnection.getSalt(username);
+        String encryptor = Encryptor.Encryptor(password, salt);
+        String hash = Encryptor.getHash(encryptor);
+        String columnName = "password";
+        boolean test = DBConnection.exists(columnName, hash);
+        boolean expResult = true;
+        assertEquals(expResult, test);
+    }
+
+    @Test
+    public void getLoggedInTest() throws Exception {
+        boolean test = DBConnection.getLoggedIn("admin");
+        boolean expResult = false;
+        assertEquals(expResult, test);
+    }
+
+    @Test
+    public void setLoggedInTest() throws Exception {
+        DBConnection.setLoggedIn("admin", 1);
+        boolean test = DBConnection.getLoggedIn("admin");
+        boolean expResult = true;
+        DBConnection.setLoggedIn("admin", 0);
+        assertEquals(expResult, test);
+    }
+
+    @Test
+    public void getUserIDTest() throws Exception {
+        String userName = "admin";
+        int test = DBConnection.getUserID(userName);
+        int expResult = 1;
+        assertEquals(expResult, test);
+
+    }
+
 }
