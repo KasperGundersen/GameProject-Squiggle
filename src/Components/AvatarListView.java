7de8803f06e9a0851c7787978ee39563671fd1d6
kaspervg <kvgun@live.no>
2019-03-12 14:56:01 +0100

    listofimages

--- src/Components/AvatarListView.java
+++ src/Components/AvatarListView.java
@@ -1,9 +1,29 @@
 package Components;
 
-import java.awt.*;
+import Database.DBConnection;
+import javafx.collections.ObservableList;
+import javafx.scene.image.Image;
+
+import javax.swing.text.html.ListView;
+import java.io.File;
+import java.util.ArrayList;
 
 public class AvatarListView {
-    private Image[] listOfImages;
+    private ArrayList<Player> players = DBConnection.getAvatarID();
+    private int amtPlayers = players.size();
+    private Image[] listOfImages = new Image[players.size()];
+   // private ListView<> listView;
+    private ObservableList<String> users;
+
+    public AvatarListView() {
+        for(int i = 0; i<amtPlayers;i++ ){
+            listOfImages[i] = getAvatar(players.get(i).getAvatarID());
+        }
+    }
 
 
+    public Image getAvatar(int i){
+        File file = new File("resources/avatars/" + i + ".jpg");
+        return new javafx.scene.image.Image(file.toURI().toString());
+    }
 }
